75c2af70a249df5b5adc08b31a3b30c2a2e0b40a
Daniel <danielcortes2001@gmail.com>
2024-10-28 20:08:12 +0100

    Added is_admin field to User model

--- profile-service/src/models/user_model.py
+++ profile-service/src/models/user_model.py
@@ -8,6 +8,7 @@ class User(Model):
     biography = fields.TextField(null=True)
     profile_picture = fields.TextField(default='default_profile_picture.jpg')
     stream_key = fields.CharField(max_length=255, unique=True, null=True)
+    is_admin = fields.BooleanField(default=False)
 
     class Meta:
         table = "users"
