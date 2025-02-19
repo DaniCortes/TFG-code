5d840312861c1a1a125ac96208409429a1446d49
Daniel <danielcortes2001@gmail.com>
2025-02-19 17:28:46 +0100

    Modified login-deploy.sh file

--- login-service/login-deploy.sh
+++ login-service/login-deploy.sh
@@ -2,11 +2,32 @@
 
 cd $(dirname $(readlink -f $0))
 
-echo "Aplicando despliegue del servicio de login"
+eval $(minikube docker-env)
+
+
+echo "Eliminando posible despliegue anterior"
+kubectl delete -f ./deployment.yaml
+kubectl delete -f ./service.yaml
+kubectl delete -f ./httproute.yaml
+
+echo "Esperando a que el despliegue se complete"
+kubectl rollout status deployment/login-service
+
+
+echo "Eliminando imagen de servicio de edición de perfiles"
+sleep 1.5
+docker rmi login-service:latest
+
+echo "Construyendo imagen de servicio de edición de perfiles"
+docker build -t login-service:latest .
+
+eval $(minikube docker-env -u)
+
+echo "Aplicando despliegue del servicio de inicio de sesión"
 kubectl apply -f ./deployment.yaml
 
-echo "Aplicando servicio de login"
+echo "Aplicando servicio de inicio de sesión"
 kubectl apply -f ./service.yaml
 
-echo "Aplicando rutas de login"
+echo "Aplicando rutas de inicio de sesión"
 kubectl apply -f ./httproute.yaml
--- transmux-service/src/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from routes import transmuxing_routes
-
-
-app = FastAPI("Transmuxing Service")
-
-app.include_router(transmuxing_routes.router)
